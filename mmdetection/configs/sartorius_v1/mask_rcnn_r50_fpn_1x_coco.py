7e506890c957868b7a25ad3f74b10b0134220076
Kota Noda <kouta.tus1@gmail.com>
2021-10-20 18:14:26 +0900

    change mask_rcnn_r50_fpn_1x_coco.py

--- mmdetection/configs/sartorius_v1/mask_rcnn_r50_fpn_1x_coco.py
+++ mmdetection/configs/sartorius_v1/mask_rcnn_r50_fpn_1x_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
-    './base/models/mask_rcnn_r50_fpn.py',
-    './base/datasets/sartorius_instance.py',
-    './base/schedules/schedule_1x.py', 
+    './base/mask_rcnn_r50_fpn.py',
+    './base/sartorius_instance.py',
+    './base/schedule_1x.py', 
     './base/default_runtime.py'
 ]
