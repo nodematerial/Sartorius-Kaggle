ac29937149a2f0969318c7ec899e13ef348e2bf9
Kota Noda <kouta.tus1@gmail.com>
2021-10-23 20:50:40 +0900

    disabled interval

--- mmdetection/configs/sartorius_v2/base/sartorius_instance.py
+++ mmdetection/configs/sartorius_v2/base/sartorius_instance.py
@@ -46,4 +46,4 @@ data = dict(
         ann_file=data_root + 'test.pkl',
         img_prefix=data_root + 'test/',
         pipeline=test_pipeline))
-evaluation = dict(metric=['mAP']) #bbox
+evaluation = dict(interval=1000) #bbox
