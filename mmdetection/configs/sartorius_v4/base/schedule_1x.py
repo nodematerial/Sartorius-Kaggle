a5cdda2f942a57f721d523c08b76433c551e64c7
Kota Noda <kouta.tus1@gmail.com>
2021-10-26 17:36:52 +0900

    changed runtime and scheduler

--- mmdetection/configs/sartorius_v4/base/schedule_1x.py
+++ mmdetection/configs/sartorius_v4/base/schedule_1x.py
@@ -1,5 +1,6 @@
 # optimizer
-optimizer = dict(type='Adam', lr=0.005, weight_decay=0.0001)
+optimizer = dict(type='Adam', lr=0.0002, weight_decay=0.0001)
+#optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
