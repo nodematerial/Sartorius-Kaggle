b91a55a3c39629d2c0bac3d8583f4287f5046c87
Kota Noda <kouta.tus1@gmail.com>
2021-10-26 17:28:46 +0900

    modified v4

---
+++ mmdetection/configs/sartorius_v4/cascade_mask_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_base_/models/cascade_mask_rcnn_r50_fpn.py',
+    '/base/sartorius_instance.py',
+    '/base/schedule_1x.py', 
+    '/base/default_runtime.py'
+]
--- mmdetection/configs/sartorius_v4/mask_rcnn_r50_fpn_1x_coco.py
+++
@@ -1,6 +0,0 @@
-_base_ = [
-    './base/mask_rcnn_r50_fpn.py',
-    './base/sartorius_instance.py',
-    './base/schedule_1x.py', 
-    './base/default_runtime.py'
-]
