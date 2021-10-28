36da465b114e7533e07a4a8ef593c6c96c469028
Kota Noda <kouta.tus1@gmail.com>
2021-10-28 22:20:50 +0900

    changed v3

--- mmdetection/configs/sartorius_v3/base/sartorius_instance.py
+++ mmdetection/configs/sartorius_v3/base/sartorius_instance.py
@@ -1,13 +1,12 @@
-# dataset settings
 dataset_type = 'CocoDataset'
 classes = ('True',) # Added
-data_root = '../work/'
+data_root = '../work/LIVECell_dataset_2021/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(520, 704), keep_ratio=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -18,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(520, 704),
+        img_scale=(1333, 800),
         flip=True,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -34,20 +33,20 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'train.json',
-        img_prefix=data_root + 'train/',
+        ann_file=data_root + 'annotations/train.json',
+        img_prefix=data_root + 'images/livecell_train_val_images',
         classes=classes, # Added
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'val.json',
-        img_prefix=data_root + 'train/',
+        ann_file=data_root + 'annotations/val.json',
+        img_prefix=data_root + 'images/livecell_train_val_images',
         classes=classes, # Added
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'test.json',
-        img_prefix=data_root + 'test/',
+        ann_file=data_root + 'annotations/test.json',
+        img_prefix=data_root + 'images/livecell_test_images',
         classes=classes, # Added
         pipeline=test_pipeline))
-evaluation = dict(metric=['bbox', 'segm']) 
+evaluation = dict(metric=['segm']) 
